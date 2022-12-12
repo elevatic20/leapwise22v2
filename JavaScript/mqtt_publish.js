89657a7aa9d5b099abc50f473e7fbf27c2a56138
Emanuel Levatić <elevatic20@foi.hr>
2022-12-12 23:00:31 +0100

    broker port 8000

--- JavaScript/mqtt_publish.js
+++ JavaScript/mqtt_publish.js
@@ -1,7 +1,7 @@
 var mqtt;
 var reconnectTimeout = 2000;
 var host = "broker.hivemq.com";
-var port = 1883;
+var port = 8000;
 
             function onConnect() {
                 console.log("Connected");
