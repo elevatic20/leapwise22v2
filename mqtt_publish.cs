59680af8c7de57b55b49f40a8a26f4d6677bd6a3
Karlo Međimorec <100922130+kmedimore20@users.noreply.github.com>
2022-12-22 07:42:27 +0000

    Moved mqtt communication to c#

---
+++ mqtt_publish.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Text;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace MqttTest
+{
+    class Mqtt_publish
+    {
+        MqttClient client;
+        private void mqttConnect(){
+            client = new MqttClient("broker.hivemq.com");
+            client.Connect("newID");
+        }
+
+        private void mqttPublish(string message){
+            client.Publish(Encoding.UTF8.GetBytes(message));
+        }
+
+        private void mqttDisconnect(){
+            client.Disconnect();
+        }
+    }
+}
