0726b8918ba3359bc464a45df2e682fd91ece8ad
tsisko20 <55455483+tsisko20@users.noreply.github.com>
2022-12-30 18:46:28 +0100

    Update graf.js

--- JavaScript/graf.js
+++ JavaScript/graf.js
@@ -4,27 +4,29 @@ async function getData(){
                         const response = await fetch("https://lampicabackendapi.azurewebsites.net/data");
                         const data = await response.json();
                         console.log(data);
-                   }
+                        new Chart(ctx, {
+                            type: 'line',
+                            data: {
+                              labels: ['Ponedjeljak', 'Utorak', 'Srijeda', 'Četvrtak', 'Petak', 'Subota', 'Nedjelja'],
+                              datasets: [{
+                                label: '# sati rada Lampice',
+                                data: [data.Monday, data.Tuesday, data.Wednedsay, data.Thursday, data.Friday, data.Saturday, data.Sunday],
+                                borderColor: 'rgb(75, 192, 192)',
+                                borderWidth: 4,
+                                tension: 0.1,
+
+                              }]
+                            },
+                            options: {
+                              scales: {
+                                y: {
+                                  beginAtZero: true
+                                }
+                              }
+                            }
+                          });
+
+                        }
                    getData();
 
-  new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: ['Ponedjeljak', 'Utorak', 'Srijeda', 'Četvrtak', 'Petak', 'Subota', 'Nedjelja'],
-      datasets: [{
-        label: '# sati rada Lampice',
-        data: [data.Monday, data.Tuesday, data.Wednedsay, data.Thursday, data.Friday, data.Saturday, data.Sunday],
-        borderColor: 'rgb(75, 192, 192)',
-        borderWidth: 4,
-        tension: 0.1,
-        
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
-    }
-  });
+  
