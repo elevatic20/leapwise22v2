52a5bad72c1b59ced3c75bc43056618a691c025f
Karlo Međimorec <100922130+kmedimore20@users.noreply.github.com>
2022-12-30 15:07:48 +0100

    Update main.js

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -15,7 +15,6 @@ const cb = document.querySelector('#klizac');
                             request.onload = () => {
                                     if (request.status === 200){
                                     console.log(JSON.parse(request.response));
-                                    }
                             } else {
                                     console.log(request)
                                     console.log('error ${request.status}')
