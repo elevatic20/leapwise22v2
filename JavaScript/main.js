9452121668df3117e7d4c59036c7f4accf6db3fe
Karlo Međimorec <100922130+kmedimore20@users.noreply.github.com>
2022-12-30 15:41:40 +0100

    Update main.js

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -5,6 +5,19 @@ const cb = document.querySelector('#klizac');
                 if(cb.checked == true){
                     document.querySelector(':root').style.setProperty('--main-color' , "#ffffff");
                     // graf.style.display = "block";
+                    const RequestDecompressionBuilderExtensions = new XMLHttpRequest();
+                    request.open("POST", "https://lampicabackendapi.azurewebsites.net/ledOn" );
+                    request.send();
+                    request.onload = () =>{
+                        if(Request.status === 200){
+                            console.log("Ubacen podatak u tablicu!");
+                        }
+                        else{
+                            console.log(request);
+                            console.log('error ${request.status}');
+                        }
+                    }
+
                     document.querySelector('.switcher-btn').onclick = () =>{
                         document.querySelector('.color-switcher').classList.toggle('active');
                     };
@@ -13,10 +26,10 @@ const cb = document.querySelector('#klizac');
 
                     themeButtons.forEach(color =>{
                         color.addEventListener('click', ()=>{
-                        let dataColor = color.getAttribute('data-color');
-                        document.querySelector(':root').style.setProperty('--main-color' , dataColor);
+                            let dataColor = color.getAttribute('data-color');
+                            document.querySelector(':root').style.setProperty('--main-color' , dataColor);
+                        });
                     });
-                });
 
                 }else{
                     document.querySelector(':root').style.setProperty('--main-color' , "#808080");
@@ -26,4 +39,4 @@ const cb = document.querySelector('#klizac');
                         document.querySelector('.color-switcher').classList.remove('active');
                     };
                 }
-            });
+        });
