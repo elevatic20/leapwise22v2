d077bc5454b0346d6fea9d4b91ef4b5af49b6f65
tsisko20 <55455483+tsisko20@users.noreply.github.com>
2022-12-30 21:29:17 +0100

    Update main.js

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -9,29 +9,29 @@ const cb = document.querySelector('#klizac');
                     request.open("POST", "https://lampicabackendapi.azurewebsites.net/ledOn" );
                     request.send();
                     request.onload = () =>{
-                        if(request.status === 200){
-                            console.log("Lampica upaljena!");
+                        if(Request.status === 200){
+                            console.log("Ubacen podatak u tablicu!");
                         }
                         else{
                             console.log(request);
                             console.log('error ${request.status}');
                         }
                     }
-                   
+
+                    document.querySelector('.switcher-btn').onclick = () =>{
+                        document.querySelector('.color-switcher').classList.toggle('active');
+                    };
+
+                    let themeButtons = document.querySelectorAll('.theme-buttons');
+
+                    themeButtons.forEach(color =>{
+                        color.addEventListener('click', ()=>{
+                            let dataColor = color.getAttribute('data-color');
+                            document.querySelector(':root').style.setProperty('--main-color' , dataColor);
+                        });
+                    });
+
                 }else{
-                    const request = new XMLHttpRequest();
-                    request.open("POST", "https://lampicabackendapi.azurewebsites.net/ledOff" );
-                    request.send();
-                    request.onload = () =>{
-                        if(request.status === 200){
-                            console.log("Lampica ugasena!");
-                        }
-                        else{
-                            console.log(request);
-                            console.log('error ${request.status}');
-                        }
-                    }
-                    
                     document.querySelector(':root').style.setProperty('--main-color' , "#808080");
                     document.querySelector('.color-switcher').classList.remove('active');
                     // graf.style.display = "none";
@@ -40,109 +40,3 @@ const cb = document.querySelector('#klizac');
                     };
                 }
         });
-
-
-document.getElementById('red').onclick = function () {
-        const request = new XMLHttpRequest();
-                    request.open("POST", "https://lampicabackendapi.azurewebsites.net/redOn");
-                    request.send();
-                    request.onload = () =>{
-                        if(request.status === 200){
-                            console.log("Boja je promijenjena na crvenu!");
-                        }
-                        else{
-                            console.log(request);
-                            console.log('error ${request.status}');
-                        }
-                    }
-}
-
-document.getElementById('green').onclick = function () {
-        const request = new XMLHttpRequest();
-                    request.open("POST", "https://lampicabackendapi.azurewebsites.net/greenOn");
-                    request.send();
-                    request.onload = () =>{
-                        if(request.status === 200){
-                            console.log("Boja je promijenjena na zelenu!");
-                        }
-                        else{
-                            console.log(request);
-                            console.log('error ${request.status}');
-                        }
-                    }
-}
-
-document.getElementById('blue').onclick = function () {
-        const request = new XMLHttpRequest();
-                    request.open("POST", "https://lampicabackendapi.azurewebsites.net/blueOn");
-                    request.send();
-                    request.onload = () =>{
-                        if(request.status === 200){
-                            console.log("Boja je promijenjena na plavu!");
-                        }
-                        else{
-                            console.log(request);
-                            console.log('error ${request.status}');
-                        }
-                    }
-}
-
-document.getElementById('yellow').onclick = function () {
-        const request = new XMLHttpRequest();
-                    request.open("POST", "https://lampicabackendapi.azurewebsites.net/yellowOn");
-                    request.send();
-                    request.onload = () =>{
-                        if(request.status === 200){
-                            console.log("Boja je promijenjena na zutu!");
-                        }
-                        else{
-                            console.log(request);
-                            console.log('error ${request.status}');
-                        }
-                    }
-}
-
-document.getElementById('purple').onclick = function () {
-        const request = new XMLHttpRequest();
-                    request.open("POST", "https://lampicabackendapi.azurewebsites.net/purpleOn");
-                    request.send();
-                    request.onload = () =>{
-                        if(request.status === 200){
-                            console.log("Boja je promijenjena na ljubičastu!");
-                        }
-                        else{
-                            console.log(request);
-                            console.log('error ${request.status}');
-                        }
-                    }
-}
-
-document.getElementById('cyan').onclick = function () {
-        const request = new XMLHttpRequest();
-                    request.open("POST", "https://lampicabackendapi.azurewebsites.net/cyanOn");
-                    request.send();
-                    request.onload = () =>{
-                        if(request.status === 200){
-                            console.log("Boja je promijenjena na cijan!");
-                        }
-                        else{
-                            console.log(request);
-                            console.log('error ${request.status}');
-                        }
-                    }
-}
-
-document.getElementById('white').onclick = function () {
-        const request = new XMLHttpRequest();
-                    request.open("POST", "https://lampicabackendapi.azurewebsites.net/whiteOn");
-                    request.send();
-                    request.onload = () =>{
-                        if(request.status === 200){
-                            console.log("Boja je promijenjena na bijelu!");
-                        }
-                        else{
-                            console.log(request);
-                            console.log('error ${request.status}');
-                        }
-                    }
-}
