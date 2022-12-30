ec95cca3a891c155431c682abf8bdeeef2e40ce7
tsisko20 <55455483+tsisko20@users.noreply.github.com>
2022-12-30 18:42:49 +0100

    Update graf.js

--- JavaScript/graf.js
+++ JavaScript/graf.js
@@ -1,12 +1,19 @@
 const ctx = document.getElementById('myChart');
 
+async function getData(){ 
+                        const response = await fetch("https://lampicabackendapi.azurewebsites.net/data");
+                        const data = await response.json();
+                        console.log(data);
+                   }
+                   getData();
+
   new Chart(ctx, {
     type: 'line',
     data: {
       labels: ['Ponedjeljak', 'Utorak', 'Srijeda', 'Četvrtak', 'Petak', 'Subota', 'Nedjelja'],
       datasets: [{
         label: '# sati rada Lampice',
-        data: [5, 20, 3, 8, 14, 7, 13],
+        data: [data.Monday, data.Tuesday, data.Wednedsay, data.Thursday, data.Friday, data.Saturday, data.Sunday],
         borderColor: 'rgb(75, 192, 192)',
         borderWidth: 4,
         tension: 0.1,
